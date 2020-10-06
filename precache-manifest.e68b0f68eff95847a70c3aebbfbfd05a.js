91dab2386a029b60bc6a51ad874d23b2ca6d3d48
Ikraam <ikraamghoor@gmail.com>
2020-10-06 14:05:11 +0200

    Updates

---
+++ precache-manifest.e68b0f68eff95847a70c3aebbfbfd05a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458819dffd3be03f0c6067a7b7582327",
+    "url": "/ikraamg/react-redux.git/index.html"
+  },
+  {
+    "revision": "5b0caadd2ce13f26c011",
+    "url": "/ikraamg/react-redux.git/static/css/main.61a42a9d.chunk.css"
+  },
+  {
+    "revision": "1a577bbf88099f4497f4",
+    "url": "/ikraamg/react-redux.git/static/js/2.2548094d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ikraamg/react-redux.git/static/js/2.2548094d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0caadd2ce13f26c011",
+    "url": "/ikraamg/react-redux.git/static/js/main.ae38c9b7.chunk.js"
+  },
+  {
+    "revision": "33916bdbae25499ac503",
+    "url": "/ikraamg/react-redux.git/static/js/runtime-main.134381db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ikraamg/react-redux.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
