667188823f55fffd4667cec7aa35d7f9f3b9a339
Ikraam <ikraamghoor@gmail.com>
2020-10-08 09:38:11 +0200

    Updates

--- precache-manifest.f40f5d8c5214331a40fabd0731788c28.js
+++ precache-manifest.f40f5d8c5214331a40fabd0731788c28.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8eaae32b5ba11faeed7ca728ba04e296",
+    "revision": "1e4016c0a463218f0ffd175a499dbbd1",
     "url": "/react-redux/index.html"
   },
   {
-    "revision": "3d508d5f6c4470a0be48",
+    "revision": "d4c1e3eed310cfa1ab29",
     "url": "/react-redux/static/css/main.3e4d0461.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-redux/static/js/2.623dfcec.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3d508d5f6c4470a0be48",
-    "url": "/react-redux/static/js/main.ab6590ca.chunk.js"
+    "revision": "d4c1e3eed310cfa1ab29",
+    "url": "/react-redux/static/js/main.5d50021c.chunk.js"
   },
   {
     "revision": "5a9c543c7ec023020fa6",
